42d85a3627fc5676d791999141a437a6af83a703
jinhongxian <jinhongxian@users.noreply.github.com>
2020-05-30 13:27:50 +0800

    fix BUG: if logic

--- warrior/reflection.lua
+++ warrior/reflection.lua
@@ -1,15 +1,19 @@
 -- Init
-aura_env.player_name = GetUnitName("player", false)
 chat_types = { "SAY", "YELL", "PARTY", "GUILD", "RAID" }
-aura_env.char_type = chat_types[aura_env.config["chat_type"]]
-if aura_env.config["language"] == "chinese" then
+languages = { "chinese", "english" }
+
+aura_env.player_name = GetUnitName("player", false)
+aura_env.chat_type = chat_types[aura_env.config["chat_type"]]
+aura_env.language = languages[aura_env.config["language"]]
+
+if aura_env.language == "chinese" then
     aura_env.chat_cast_success = "开启盾反！"
     aura_env.chat_report_1 = "盾反："
     aura_env.chat_report_2 = " 伤害："
     aura_env.chat_unit_scale = 0.0001
     aura_env.chat_unit = "万"
 else
-    aura_env.chat_cast_success = "Cast Spell Reflection!"
+    aura_env.chat_cast_success = "Spell Reflection!"
     aura_env.chat_report_1 = "Reflect: "
     aura_env.chat_report_2 = " Damage: "
     aura_env.chat_unit_scale = 0.001
@@ -18,16 +22,18 @@ end
 
 -- Trigger
 function()
-    local timestamp, event, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellId, spellName, _, a15 = CombatLogGetCurrentEventInfo()
-    if spellId == 23920 and event == "SPELL_CAST_SUCCESS" and sourceName == aura_env.player_name then
-        SendChatMessage(aura_env.cast_success, aura_env.char_type)
-        aura_env.reflect_spellIds = {}
-    elseif a15 == "REFLECT" and event == "SPELL_MISSED" and destName == aura_env.player_name then
-        aura_env.reflect_spellIds[spellId] = true
-    elseif sourceGUID == destGUID and event == "SPELL_DAMAGE" then
-        if aura_env.reflect_spellIds[spellId] then
-            link = GetSpellLink(spellId)
-            SendChatMessage(aura_env.chat_report_1 .. sourceName .." " .. link .. aura_env.chat_report_2 .. string.format("%.1f",  a15 * aura_env.chat_unit_scale) .. aura_env.chat_unit, aura_env.char_type)
+    local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool, a15, a16,a17,a18,a19,a20, a21, a22, a23, a24, a25 = CombatLogGetCurrentEventInfo()
+
+    if event == "SPELL_CAST_SUCCESS" and spellId == 23920 then
+        SendChatMessage(aura_env.chat_cast_success, aura_env.char_type)
+    elseif event == "SPELL_MISSED" and destName == aura_env.player_name and a15 == "REFLECT" then
+        aura_env.reflect_spellId = spellId
+    elseif event == "SPELL_DAMAGE" then
+        if sourceGUID == destGUID then
+            if aura_env.reflect_spellId == spellId then
+                link = GetSpellLink(spellId)
+                SendChatMessage(aura_env.chat_report_1 .. sourceName .." " .. link .. aura_env.chat_report_2 .. string.format("%.1f",  a15 * aura_env.chat_unit_scale) .. aura_env.chat_unit, aura_env.chat_type)
+            end
         end
     end
 end
--- warrior/twilight.lua
+++ warrior/twilight.lua
@@ -1,14 +1,18 @@
 -- Init
-aura_env.player_name = GetUnitName("player", false)
-aura_env.damage = 0;
-aura_env.count = 0;
-aura_env.start_time = 0;
 chat_types = { "SAY", "YELL", "PARTY", "GUILD", "RAID" }
-aura_env.char_type = chat_types[aura_env.config["chat_type"]]
-if aura_env.config["language"] == "chinese" then
+languages = { "chinese", "english" }
+
+aura_env.player_name = GetUnitName("player", false)
+aura_env.damage = 0
+aura_env.count = 0
+aura_env.start_time = 0
+aura_env.chat_type = chat_types[aura_env.config["chat_type"]]
+aura_env.language = languages[aura_env.config["language"]]
+
+if aura_env.language == "chinese" then
     aura_env.chat_cast_success = "开炮！！！"
     aura_env.chat_report_1 = " -> 击中"
-    aura_env.chat_report_2 = "目标， 造成伤害："
+    aura_env.chat_report_2 = "个目标， 造成伤害："
     aura_env.chat_unit_scale = 0.0001
     aura_env.chat_unit = "万"
 else
@@ -20,24 +24,21 @@ else
 end
 
 -- Trigger
-function(_, current_timestamp, _)
+function()
     local timestamp, event, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellId, spellName, _, a15 = CombatLogGetCurrentEventInfo()
-
-    if  spellId == 317159 and event == "SPELL_DAMAGE" and sourceName == aura_env.player_name then
+    if event == "SPELL_DAMAGE" and sourceName == aura_env.player_name and spellId == 317159 then
         if aura_env.count == 0 then
-            SendChatMessage(aura_env.chat_cast_success , aura_env.char_type)
+            SendChatMessage(aura_env.chat_cast_success , aura_env.chat_type)
             aura_env.start_time = timestamp
             aura_env.spellName = spellName
         end
-
-        aura_env.count = aura_env.count + 1;
+        aura_env.count = aura_env.count + 1
         aura_env.damage = aura_env.damage + a15
-
-    elseif aura_env.count > 0 and (current_timestamp - aura_env.start_time) > 2 then
+    elseif aura_env.count > 0 and (timestamp - aura_env.start_time) > 2 then
         if aura_env.damage > 0 then
-            SendChatMessage(aura_env.spellName .. aura_env.chat_report_1 .. aura_env.count .. aura_env.chat_report_2 .. string.format("%.1f", aura_env.damage * aura_env.chat_unit_scale) .. aura_env.chat_unit , aura_env.char_type)
+            SendChatMessage(aura_env.spellName .. aura_env.chat_report_1 .. aura_env.count .. aura_env.chat_report_2 .. string.format("%.1f", aura_env.damage * aura_env.chat_unit_scale) .. aura_env.chat_unit , aura_env.chat_type)
         end
-        aura_env.count = 0;
-        aura_env.damage = 0;
+        aura_env.count = 0
+        aura_env.damage = 0
     end
 end
